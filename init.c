ab826fffc4e4b206fcfcf97bb19aa243ccf52f93
Josen <hyt@mail.ustc.edu.cn>
2018-04-18 15:23:08 +0800

    pipe complete

--- init.c
+++ init.c
@@ -15,30 +15,34 @@ int main() {
     char *args[MAX_PIPE][128];
 
     while (1) {
-        int pipes = 0;
+        int pipes = 0;          //管道数目
         fflush(stdout);
+
         /* 提示符 */
         printf("# ");
         fflush(stdin);
         fgets(cmd, 256, stdin);
+
         /* 清理结尾的换行符 */
         int i;
         for (i = 0; cmd[i] != '\n'; i++)
             ;
         cmd[i] = '\0';
-        /**/
-        for (i = 0; cmd[i] == ' '; i++)
+        /* 清除前导空格 */
+        for (i = 0; cmd[i] == ' ' || cmd[i] == '\t'; i++)
             ;
+
         /* 拆解命令行 */
         args[pipes][0] = &cmd[i];
         for (i = 0; *args[pipes][i]; i++) {
             for (args[pipes][i+1] = args[pipes][i] + 1; *args[pipes][i+1]; args[pipes][i+1]++) {
-                if (*args[pipes][i+1] == ' ') {
+                if (*args[pipes][i+1] == ' ' || *args[pipes][i+1] == '\t') {
                     *args[pipes][i+1] = '\0';
-                    /*消除多余空格*/
+
+                    /* 消除多余空格 */
                     do {
                     args[pipes][i+1]++;
-                    } while (*args[pipes][i+1] == ' ');
+                    } while (*args[pipes][i+1] == ' ' || *args[pipes][i+1] == '\t');
 
                     break;
                 }
@@ -52,7 +56,7 @@ int main() {
                 args[pipes][i+1] = NULL;
 
                 pipes++;
-                while (*args[pipes][0] == ' ')
+                while (*args[pipes][0] == ' ' || *args[pipes][0] == '\t')
                     args[pipes][0]++;
                 i = -1;
             }
@@ -64,18 +68,53 @@ int main() {
             continue;
 
         /* 内建命令 */
-        if (strcmp(args[pipes][0], "cd") == 0) {
-            if (args[pipes][1])
-                chdir(args[pipes][1]);
-            continue;
+        if (pipes == 0) {
+            if (strcmp(args[pipes][0], "cd") == 0) {
+                if (args[pipes][1]) {
+                    if (chdir(args[pipes][1]) == -1) {
+                        /* chdir失败 */
+                        perror(NULL);
+                    }
+                }
+                continue;
+            }
+            if (strcmp(args[pipes][0], "pwd") == 0) {
+                char wd[4096];
+                if (getcwd(wd, 4096) == NULL) {
+                    /* getcwd失败 */
+                    perror(NULL);
+                }
+                else
+                    puts(wd);
+                continue;
+            }
+            if (strcmp(args[pipes][0], "export") == 0) {
+                int j;
+                for (i = 1; args[pipes][i] != NULL; i++) {
+                    for (j = 0; args[pipes][i][j]; j++) {
+                        if (args[pipes][i][j] == '=') {
+                            args[pipes][i][j] = '\0';
+                            if (setenv(args[pipes][i], &args[pipes][i][j + 1], 1) == -1) {
+                                /* setenv失败 */
+                                perror(NULL);
+                            }
+                            break;
+                        }
+                    }
+                }
+                continue;
+            }
+            if (strcmp(args[pipes][0], "unset") == 0) {
+                for (i = 1; args[pipes][i] != NULL; i++) {
+                    if (unsetenv(args[pipes][i]) == -1) {
+                        perror(NULL);
+                    }
+                }
+                continue;
+            }
+            if (strcmp(args[pipes][0], "exit") == 0)
+                return 0;
         }
-        if (strcmp(args[pipes][0], "pwd") == 0) {
-            char wd[4096];
-            puts(getcwd(wd, 4096));
-            continue;
-        }
-        if (strcmp(args[pipes][0], "exit") == 0)
-            return 0;
 
         /*外部命令*/
         int fildes[2];
@@ -83,13 +122,21 @@ int main() {
 
         status = pipe(fildes);
         if (status == -1 ) {
-            /* an error occurred */
-            exit(0);
+            /* 创建管道失败 */
+            perror(NULL);
+            continue;
         }
+
         exec(args, fildes, 0, pipes);
     }
 }
 
+/*
+* args为命令列表
+* fildes为与前一个进程共用的管道
+* now为当前命令在args中的索引
+* pipes为管道数
+*/
 void exec(char *args[][128], int fildes[2], int now, int pipes)
 {
     if (pipes == 0) {
@@ -98,16 +145,15 @@ void exec(char *args[][128], int fildes[2], int now, int pipes)
             /* 子进程 */
             if (execvp(args[pipes][0], args[pipes]) == -1) {
                 /* execvp失败 */
-                if (errno == EACCES) {
-                    printf("error: you cannot perform this operation unless you are root.");
-                }
-                else if (errno == ENOEXEC) {
-                    printf("bash: command not found: %s", args[pipes][0]);
-                }
+                perror(NULL);
                 exit(EXIT_FAILURE);
             }
-            
             exit(EXIT_SUCCESS);
+        }
+        else if (pid == -1) {
+            /* fork失败 */
+            perror(NULL);
+            return;
         }
         /* 父进程 */
         wait(NULL);
@@ -116,24 +162,26 @@ void exec(char *args[][128], int fildes[2], int now, int pipes)
     else if (now == 0) {
         pid_t pid = fork();
         if (pid == 0) {
-            close(fildes[0]);                       /* Read end is unused */
+            close(fildes[0]);                       /* 读入端不用 */
             dup2(fildes[1], STDOUT_FILENO);
-            close(fildes[1]);
+            close(fildes[1]);                       /* 关闭副本 */
             if (execvp(args[now][0], args[now]) == -1) {
                 /* execvp失败 */
-                if (errno == EACCES) {
-                    printf("error: you cannot perform this operation unless you are root.");
-                }
-                else if (errno == ENOEXEC) {
-                    printf("bash: command not found: %s", args[now][0]);
-                }
+                perror(NULL);
                 exit(EXIT_FAILURE);
             }
             exit(EXIT_SUCCESS);
         }
-        close(fildes[1]); 
+        else if (pid == -1) {
+            /* fork失败 */
+            perror(NULL);
+            close(fildes[1]);
+            close(fildes[0]);
+            return;
+        }
+         
         exec(args, fildes, now + 1, pipes);
-        close(fildes[0]);
+
         wait(NULL);
         return;
     }
@@ -143,58 +191,70 @@ void exec(char *args[][128], int fildes[2], int now, int pipes)
             
         status = pipe(fildes_other);
         if (status == -1 ) {
-            /* an error occurred */
-            exit(0);
+            /* 创建管道失败 */
+            perror(NULL);
+            return;
         }
 
         pid_t pid = fork();
         if (pid == 0) {
-            close(fildes[1]);                       /* Write end is unused */
+            close(fildes[1]);                       /* 前一个管道写入端不用 */
             dup2(fildes[0], STDIN_FILENO);
             close(fildes[0]);
+
+            close(fildes_other[0]);                 /* 现在的管道读入端不用 */
             dup2(fildes_other[1], STDOUT_FILENO);
             close(fildes_other[1]);
 
             if (execvp(*args[now], args[now]) == -1) {
                 /* execvp失败 */
-                if (errno == EACCES) {
-                    printf("error: you cannot perform this operation unless you are root.");
-                }
-                else if (errno == ENOEXEC) {
-                    printf("bash: command not found: %s", args[now][0]);
-                }
+                perror(NULL);
                 exit(EXIT_FAILURE);
             }
+
             exit(EXIT_SUCCESS);
         }
-        close(fildes[0]);
-        close(fildes_other[1]);
-        exec(args, fildes_other, now + 1, pipes);
+        else if (pid == -1) {
+            /* fork失败 */
+            perror(NULL);
+            close(fildes[1]);
+            close(fildes[0]);
+            close(fildes_other[0]);
+            close(fildes_other[1]);
+            return;
+        }
+        
+
+        close(fildes[0]);                           /* 后面进程不需前一个管道 */
         close(fildes[1]);
-        close(fildes_other[0]);
+
+        exec(args, fildes_other, now + 1, pipes);
+
         wait(NULL);
         return;
     }
     else if (now == pipes) {
         pid_t pid = fork();
         if (pid == 0) {
-            close(fildes[1]);                       /* Write end is unused */
+            close(fildes[1]);                       /* 写入端不用 */
             dup2(fildes[0], STDIN_FILENO);
             close(fildes[0]);
             if (execvp(args[now][0], args[now]) == -1) {
                 /* execvp失败 */
-                if (errno == EACCES) {
-                    printf("error: you cannot perform this operation unless you are root.");
-                }
-                else if (errno == ENOEXEC) {
-                    printf("bash: command not found: %s", args[now][0]);
-                }
+                perror(NULL);
                 exit(EXIT_FAILURE);
             }
             exit(EXIT_SUCCESS);
         }
-        close(fildes[0]);
+        else if (pid == -1) {
+            /* fork失败 */
+            perror(NULL);
+            close(fildes[1]);
+            close(fildes[0]);
+            return;
+        }
         close(fildes[1]);
+        close(fildes[0]);
         wait(NULL);
         return;
     }
